a1fe8ce8f268e6e4a9902a62800c5d33f93c7287
egfedo <fegor04@gmail.com>
2025-04-28 15:03:22 +0300

    feat: add all used clean day tags

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, APIRouter
+from api.user import router as user_router
 
 api_router = APIRouter(prefix="/api")
+api_router.include_router(user_router)
 
 server = FastAPI()
 server.include_router(api_router)
