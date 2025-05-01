0da6e2046c897c8f666656b7060291e59d6ab822
Kivner <108284952+Kivner@users.noreply.github.com>
2025-05-01 05:40:58 +0300

    feat: Создание страницы Субботники

---
+++ frontend/src/pages/CleandaysPage/CleandaysPage.tsx
@@ -0,0 +1,14 @@
+import './CleandaysPage.css'
+
+import * as React from 'react'
+import Typography from '@mui/material/Typography';
+
+function CleandaysPage() {
+    return (
+        <Typography variant="h4">
+            Страница "Субботники" (В разработке)
+        </Typography>
+    );
+}
+
+export default CleandaysPage;
