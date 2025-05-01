0fd4acec03f27d40b47d8b6d551ca64022e7aff4
Kivner <108284952+Kivner@users.noreply.github.com>
2025-05-01 05:40:37 +0300

    feat: Создание страницы Пользователи

---
+++ frontend/src/pages/UsersPage/UsersPage.tsx
@@ -0,0 +1,14 @@
+import './UsersPage.css'
+
+import * as React from 'react'
+import Typography from '@mui/material/Typography';
+
+function UsersPage() {
+    return (
+        <Typography variant="h4">
+            Страница "Пользователи" (В разработке)
+        </Typography>
+    );
+}
+
+export default UsersPage;
