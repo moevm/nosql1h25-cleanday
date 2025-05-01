11bc32e12388b35dbfeca45cbdaac6b86d0346a5
Kivner <108284952+Kivner@users.noreply.github.com>
2025-05-01 05:40:45 +0300

    feat: Создание страницы Статистика

---
+++ frontend/src/pages/StatisticsPage/StatisticsPage.tsx
@@ -0,0 +1,14 @@
+import './StatisticsPage.css'
+
+import * as React from 'react'
+import Typography from '@mui/material/Typography';
+
+function StatisticsPage() {
+    return (
+        <Typography variant="h4">
+            Страница "Статистика" (В разработке)
+        </Typography>
+    );
+}
+
+export default StatisticsPage;
