37b51afd0894ebe912045e941128344317f34d1a
Kivner <108284952+Kivner@users.noreply.github.com>
2025-05-11 02:30:19 +0300

    fix: Добавление совместимости

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,28 +2,38 @@ import './app.css';
 
 import * as React from 'react';
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes, useLocation} from 'react-router-dom';
 import Menu from './pages/Menu/Menu.tsx';
 import AppRoutes from './pages/AppRoutes.tsx';
-import { Box } from '@mui/material';
-import { AuthProvider } from './pages/Menu/Menu.tsx';
+import {Box} from '@mui/material';
+import {AuthProvider} from './pages/Menu/Menu.tsx';
+import Appbar from "./components/layout/Appbar.tsx";
+
+
+function AppContent() {
+
+    return (
+        <Box sx={{
+            display: 'flex',
+            mx: '10px',
+            height: '100%',
+            width: '95vw',
+        }}>
+            <Appbar/>
+            <Routes>
+                <Route path="/" element={<Menu/>}/>
+                <Route path="*" element={<AppRoutes/>}/>
+            </Routes>
+        </Box>
+    );
+}
 
 const App = () => {
     return (
         <AuthProvider>
-            <Box sx={{
-                display: 'flex',
-                mx: '10px',
-                height: '100%',
-                width: '95vw',
-            }}>
-                <Router>
-                    <Routes>
-                        <Route path="/" element={<Menu />} />
-                        <Route path="*" element={<AppRoutes />} />
-                    </Routes>
-                </Router>
-            </Box>
+            <Router>
+                <AppContent/>
+            </Router>
         </AuthProvider>
     );
 }
