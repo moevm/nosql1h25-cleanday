c85bd26795ce2fffbe9a7d20ff39a3234d42a95f
egfedo <fegor04@gmail.com>
2025-06-01 12:07:38 +0300

    feat(backend/api): add routers to main

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI, APIRouter
 from api.user import router as user_router
 from api.cleanday import router as cleanday_router
+from api.auth import router as auth_router
+from api.stats import router as stats_router
+from api.city import router as city_router
+
 
 api_router = APIRouter(prefix="/api")
 api_router.include_router(user_router)
 api_router.include_router(cleanday_router)
+api_router.include_router(auth_router)
+api_router.include_router(stats_router)
+api_router.include_router(city_router)
 
 server = FastAPI()
 server.include_router(api_router)
