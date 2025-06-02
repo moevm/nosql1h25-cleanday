273b6ecee58a8985c6ffed6347b17492fc9d75a3
Egor Lobanov <lobanoweg@gmail.com>
2025-06-02 22:58:41 +0300

    feat(main): wrap App component with LocalizationProvider and QueryClientProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import {LocalizationProvider} from "@mui/x-date-pickers/LocalizationProvider";
+import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
