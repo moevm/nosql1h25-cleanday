394867c19e4d8ee3aaa3e9df3cc072918fc29935
pitatir <alianocte@yandex.ru>
2025-06-05 03:37:20 +0300

    feat(hooks): created template to get one hooks

---
+++ frontend/src/hooks/useGetOneTemplate.tsx
@@ -0,0 +1,29 @@
+import {QueryKey, useQuery, UseQueryOptions} from '@tanstack/react-query';
+import {AxiosRequestConfig} from "axios";
+
+import axiosInstance from "@/axiosInstance";
+
+import {BaseApiModel} from "@api/BaseApiModel.ts";
+import {BaseModel} from "@models/BaseModel.ts";
+
+/**
+ * Универсальный хук для GET-запросов, использующий useQuery, с поддержкой пагинации и трансформации:
+ */
+export function useGetOneTemplate<ApiModel = BaseApiModel, Model = BaseModel>(
+    queryKey: QueryKey,
+    url: string,
+    params?: AxiosRequestConfig['params'],
+    options?: Omit<UseQueryOptions<Model>, 'queryKey' | 'queryFn'>,
+    transform?: (data: ApiModel) => Model
+) {
+    return useQuery<Model>({
+        queryKey: [...queryKey, params],
+        queryFn: async () => {
+            const response = await axiosInstance.get<ApiModel>(url, {
+                params,
+            });
+            return transform ? transform(response.data) : (response.data as unknown as Model);
+        },
+        ...options,
+    });
+}
