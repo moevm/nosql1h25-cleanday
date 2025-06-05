a62388b8a16eefb07b8fb8b91dc49c1538ebfcf9
pitatir <alianocte@yandex.ru>
2025-06-05 03:32:35 +0300

    feat(models): created Location model

---
+++ frontend/src/models/Location.ts
@@ -0,0 +1,9 @@
+import BaseModel from './BaseModel';
+
+import {City} from "./City";
+
+export interface Location extends BaseModel {
+    address: string;
+    instructions: string;
+    city: City;
+}
