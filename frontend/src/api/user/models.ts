6f313b3d13e0d5386aedde5689f6df0f7a3e6267
pitatir <alianocte@yandex.ru>
2025-06-05 03:28:38 +0300

    feat(api/user/models): created Sex enum and GetUsersParams model. removed GetUsersResponse

--- frontend/src/api/user/models.ts
+++ frontend/src/api/user/models.ts
@@ -1,11 +1,17 @@
-import {BaseApiModel, BaseGetResponseModel} from "@api/BaseApiModel";
+import {BaseApiModel, BaseGetParamsModel} from "@api/BaseApiModel";
+
+export enum Sex {
+    male = "male",
+    female = "female",
+    other = "other",
+}
 
 
 export interface UserApiModel extends BaseApiModel {
     first_name: string;
     last_name: string;
     login: string;
-    sex: string;
+    sex: Sex;
     city: string;
     about_me: string;
     score: number;
@@ -17,14 +23,27 @@ export interface UserApiModel extends BaseApiModel {
     updated_at?: string;
 }
 
-export interface GetUsersResponse extends BaseGetResponseModel {
-    users: Array<UserApiModel>;
+export interface GetUsersParams extends BaseGetParamsModel {
+    sort_by?: string;
+    first_name?: string;
+    last_name?: string;
+    login?: string;
+    sex?: Sex;
+    city?: string;
+    level_from?: string;
+    level_to?: string;
+    cleandays_from?: string;
+    cleandays_to?: string;
+    organized_from?: string;
+    organized_to?: string;
+    stat_from?: string;
+    stat_to?: string;
 }
 
 export interface UpdateUserApiModel {
     first_name?: string;
     last_name?: string;
-    sex?: string;
+    sex?: Sex;
     city_id?: string;
     about_me?: string;
     password?: string;
