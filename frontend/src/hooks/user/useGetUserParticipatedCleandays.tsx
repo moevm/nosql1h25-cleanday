75b403aa23bd925f8a20cec957b782418c177be9
baldchort <domashniy.lavrik@yandex.ru>
2025-06-22 02:34:16 +0300

    feat(frontend/hooks/user): added params as parameter in useGetUserParticipatedCleandays and useGetUserOrganizedCleandays

--- frontend/src/hooks/user/useGetUserParticipatedCleandays.tsx
+++ frontend/src/hooks/user/useGetUserParticipatedCleandays.tsx
@@ -1,16 +1,16 @@
 import {useGetPaginatedManyTemplate} from '@hooks/templates/get/useGetPaginatedManyTemplate.tsx';
-import {CleandayApiModel, GetCleandayResponse} from '@api/cleanday/models';
+import {CleandayApiModel, GetCleandayParams, GetCleandayResponse} from '@api/cleanday/models';
 import {GET_USER_CLEANDAYS} from '@api/user/endpoints.ts';
 import substituteIdToEndpoint from '@/utils/api/substituteIdToEndpoint.ts';
 import {Cleanday} from "@models/Cleanday.ts";
 import {cleandayMapper} from "@/utils/cleanday/mapper.ts";
 
-export function useGetUserParticipatedCleandays(userId: string) {
+export function useGetUserParticipatedCleandays(userId: string | number, params: GetCleandayParams) {
     return useGetPaginatedManyTemplate<CleandayApiModel, Cleanday, GetCleandayResponse>(
         ['user', userId, 'cleandays'],
         substituteIdToEndpoint(userId, GET_USER_CLEANDAYS),
         'cleandays',
-        undefined,
+        params,
         {staleTime: 5 * 60 * 1000},
         cleandayMapper
     );
