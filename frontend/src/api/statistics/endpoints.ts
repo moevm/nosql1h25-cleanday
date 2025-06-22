6b45ace257cd04dbb1febeb88cb1721411b2c7ba
Egor Lobanov <lobanoweg@gmail.com>
2025-06-22 19:10:03 +0300

    feat(api/statistics): add endpoints for user and cleanday graphs

--- frontend/src/api/statistics/endpoints.ts
+++ frontend/src/api/statistics/endpoints.ts
@@ -6,3 +6,9 @@ export const POST_IMPORT: string = '/api/stats/import';
 
 // Экспорт базы данных - GET
 export const GET_EXPORT: string = '/api/stats/export';
+
+// Получение графика по пользователям - GET
+export const GET_USERS_GRAPH: string = '/api/stats/user-heatmap';
+
+// Получение графика по субботникам - GET
+export const GET_CLEANDAY_GRAPH: string = '/api/stats/cleanday-heatmap';
